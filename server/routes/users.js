6266dfdfe63a77bd87190564645c6af3b0a5448d
wontaeklee <54618293+wontaeklee@users.noreply.github.com>
2021-03-11 14:14:41 +0900

    users Routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -26,7 +26,7 @@ router.post("/register", (req, res) => {
     const user = new User(req.body);
 
     user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
+        if (err) return res.json({ success: false, err }); 
         return res.status(200).json({ 
             success: true
         });
