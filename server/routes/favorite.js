1d0a9523a6254cc82d95c768c4f345b80cae41aa
wontaeklee <54618293+wontaeklee@users.noreply.github.com>
2021-03-11 14:13:19 +0900

    Favorite Routes

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -4,7 +4,7 @@ const { Favorite } = require('../models/Favorite')
 
 
 router.post('/favoriteNumber', (req,res) => {    
-
+ 
     // mongoDB에서 favorite 숫자 가져오기
 
         Favorite.find({'movieId' : req.body.movieId})
